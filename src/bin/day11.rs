61058be3d2de08d5b1957a49c5c463f2b391b560
al-nf <alankjfung@gmail.com>
2025-01-08 00:14:07 -0800

    d11

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -30,26 +30,136 @@ fn part1(data: &[Vec<char>]) {
     let mut row_empty: Vec<bool> = Vec::new();
     let mut col_empty: Vec<bool> = Vec::new();
 
-    // cosmic expansion: vertical
     for row in data {
         let mut has_galaxy = false;
         for ch in row {
             if *ch == '#' {has_galaxy = true;}
         }
-        if has_galaxy {
-            row_empty.push(false);
-        } else {
-            row_empty.push(true);
-        }
+        row_empty.push(!has_galaxy)
     }
 
     for i in 0..data[0].len() {
         let mut has_galaxy = false;
-        for 
+        for j in 0..data.len() {
+            if data[j][i] == '#' {
+                has_galaxy = true;
+                break;
+            }
+        }
+        col_empty.push(!has_galaxy); 
     }
+    for i in 0..data.len() {
+        let mut new_row: Vec<char> = Vec::new();
+        
+        for j in 0..data[i].len() {
+            new_row.push(data[i][j]);
+            if col_empty[j] {
+                new_row.push('.');
+            }
+        }
+        
+        cosmos.push(new_row);
+        
+        if row_empty[i] {
+            let mut empty_row = Vec::new();
+            for j in 0..data[i].len() {
+                empty_row.push('.');
+                if col_empty[j] {
+                    empty_row.push('.');
+                }
+            }
+            cosmos.push(empty_row);
+        }
+    }
+    
+    let mut galaxies: Vec<(usize, usize)> = Vec::new();
+    for i in 0..cosmos.len() {
+        for j in 0..cosmos[0].len() {
+            galaxies.push((i, j));
+        }
+    }
+    let dist = |p1: (usize, usize), p2: (usize, usize)| -> i32 {
+        (p2.0 as i32 - p1.0 as i32).abs() + (p2.1 as i32 - p1.1 as i32).abs()
+    };
+    
+    let mut sum: i32 = 0;
+    for i in 0..galaxies.len() {
+        for j in (i + 1)..galaxies.len() {
+            let galaxy1 = galaxies[i];
+            let galaxy2 = galaxies[j];
+
+            sum += dist(galaxy1, galaxy2);
+        }
+    }
+    println!("sum: {}", sum);
 }
 
 fn part2(data: &[Vec<char>]) {
+    let mut cosmos: Vec<Vec<char>> = Vec::new();
+    let mut row_empty: Vec<bool> = Vec::new();
+    let mut col_empty: Vec<bool> = Vec::new();
 
+    for row in data {
+        let mut has_galaxy = false;
+        for ch in row {
+            if *ch == '#' {has_galaxy = true;}
+        }
+        row_empty.push(!has_galaxy)
+    }
+
+    for i in 0..data[0].len() {
+        let mut has_galaxy = false;
+        for j in 0..data.len() {
+            if data[j][i] == '#' {
+                has_galaxy = true;
+                break;
+            }
+        }
+        col_empty.push(!has_galaxy); 
+    }
+    for i in 0..data.len() {
+        let mut new_row: Vec<char> = Vec::new();
+        
+        for j in 0..data[i].len() {
+            new_row.push(data[i][j]);
+            if col_empty[j] {
+                new_row.push('.');
+            }
+        }
+        
+        cosmos.push(new_row);
+        
+        if row_empty[i] {
+            let mut empty_row = Vec::new();
+            for j in 0..data[i].len() {
+                empty_row.push('.');
+                if col_empty[j] {
+                    empty_row.push('.');
+                }
+            }
+            cosmos.push(empty_row);
+        }
+    }
+    
+    let mut galaxies: Vec<(usize, usize)> = Vec::new();
+    for i in 0..cosmos.len() {
+        for j in 0..cosmos[0].len() {
+            galaxies.push((i, j));
+        }
+    }
+    let dist = |p1: (usize, usize), p2: (usize, usize)| -> u64 {
+        ((p2.0 as i32 - p1.0 as i32).abs() + (p2.1 as i32 - p1.1 as i32).abs() * (1000000-1)) as u64
+    };
+    
+    let mut sum: u64 = 0;
+    for i in 0..galaxies.len() {
+        for j in (i + 1)..galaxies.len() {
+            let galaxy1 = galaxies[i];
+            let galaxy2 = galaxies[j];
+
+            sum += dist(galaxy1, galaxy2);
+        }
+    }
+    println!("sum: {}", sum);
 }
 
